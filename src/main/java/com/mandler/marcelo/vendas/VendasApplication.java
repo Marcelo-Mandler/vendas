82a10504c9407c31e849a96a764d113277ab432c
Marcelo <89490181+Marcelo-Mandler@users.noreply.github.com>
2023-01-11 16:35:49 -0300

    criação das das classes de service, repository, model + application properties Development e Production

--- src/main/java/com/mandler/marcelo/vendas/VendasApplication.java
+++ src/main/java/com/mandler/marcelo/vendas/VendasApplication.java
@@ -1,5 +1,8 @@
 package com.mandler.marcelo.vendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-
 public class VendasApplication {
-
+	@Value("${application.name}")
+	private String applicationName;
 	@GetMapping ("/hello")
 	public String helloWorld() {
-		return "Hello World!";
+		return applicationName;
 	}
 
 	public static void main(String[] args) {
