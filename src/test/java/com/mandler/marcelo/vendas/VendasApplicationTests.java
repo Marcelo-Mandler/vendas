f41b9243563d87180596ef9f68b801016f3cdb36
Marcelo <89490181+Marcelo-Mandler@users.noreply.github.com>
2023-01-11 13:50:10 -0300

    first commit

---
+++ src/test/java/com/mandler/marcelo/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandler.marcelo.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
