e63b1c6b756386f8e85e85cb7d1a0c76fecfa407
Marcelo <89490181+Marcelo-Mandler@users.noreply.github.com>
2023-01-11 16:35:49 -0300

    criação das das classes de service, repository, model + application properties Development e Production

---
+++ src/main/java/com/mandler/marcelo/vendas/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package com.mandler.marcelo.vendas.repository;
+
+import com.mandler.marcelo.vendas.model.Client;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientRepository {
+    public void persistir(Client client) {
+        //acessa a base e salva o cliente
+
+    }
+}
