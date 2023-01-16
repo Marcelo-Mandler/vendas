10d302a1bc1456789aa2bfd641541bf59896c1b6
Marcelo <89490181+Marcelo-Mandler@users.noreply.github.com>
2023-01-16 11:11:49 -0300

    crud de clientes com jdbc

---
+++ src/main/java/com/mandler/marcelo/vendas/domain/entity/ItemPedido.java
@@ -0,0 +1,8 @@
+package com.mandler.marcelo.vendas.domain.entity;
+
+public class ItemPedido {
+    private Integer id;
+    private Pedido pedido;
+    private Produto produto;
+    private Integer quantidade;
+}
---
+++ src/main/java/com/mandler/marcelo/vendas/domain/entity/Produto.java
@@ -0,0 +1,9 @@
+package com.mandler.marcelo.vendas.domain.entity;
+
+import java.math.BigDecimal;
+
+public class Produto {
+    private Integer id;
+    private String descricao;
+    private BigDecimal precoUnitatio;
+}
